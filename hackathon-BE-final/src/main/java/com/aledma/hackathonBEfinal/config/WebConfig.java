2b1438c0171a6418e46ef43c5259afc25cc3eea4
Jeoongu <ms235280@naver.com>
2023-08-15 13:41:11 +0900

    fix : CORS add

--- hackathon-BE-final/src/main/java/com/aledma/hackathonBEfinal/config/WebConfig.java
+++ hackathon-BE-final/src/main/java/com/aledma/hackathonBEfinal/config/WebConfig.java
@@ -7,18 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /*
 CORS(Cross-Origin Resource Sharing) : 프론트와 백엔드 간의 접근 권한이 달라서 발생하는 보안 메커니즘
 이 파일은 CORS에러 해결을 위해 작성됨.
-수정이 필요함
  */
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     // allowedOrigins에 프론트 서버 도메인이 들어가면 될거 같은 느낌?
+    // 현재는 전부 허용
 
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("https://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowCredentials(true);
     }
